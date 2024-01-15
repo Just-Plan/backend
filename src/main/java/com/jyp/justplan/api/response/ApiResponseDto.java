dde5933be4307d9aa3922d0dd5fbf78f2c80dd1f
hstla <ree7243@gmail.com>
2024-01-15 18:18:43 +0900

    feat: jus-48-S3설정
    s3설정파일과 테스트코드 추가

---
+++ src/main/java/com/jyp/justplan/api/response/ApiResponseDto.java
@@ -0,0 +1,53 @@
+package com.jyp.justplan.api.response;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ApiResponseDto<T> {
+    private int code;
+    private T data;
+    private String message;
+
+    private ApiResponseDto(int code, T data, String message) {
+        this.code = code;
+        this.data = data;
+        this.message = message;
+    }
+    public static <T> ApiResponseDto<T> successResponse(T data) {
+        return new ApiResponseDto<>(ResponseCode.SUCCESS.getCode(), data, null);
+    }
+
+    public static ApiResponseDto<?> successWithoutDataResponse() {
+        return new ApiResponseDto<>(ResponseCode.SUCCESS.getCode(), null, null);
+    }
+
+    public static ApiResponseDto<?> errorResponse(ResponseCode code) {
+        return new ApiResponseDto<>(code.getCode(), null, code.getMessage());
+    }
+
+    public static ApiResponseDto<?> exceptionResponse(ResponseCode code, String message) {
+        return new ApiResponseDto<>(code.getCode(), null, message);
+    }
+
+    public static ApiResponseDto<?> failResponse(BindingResult bindingResult) {
+        Map<String, String> errors = new HashMap<>();
+
+        bindingResult.getAllErrors().forEach(error -> {
+            if (error instanceof FieldError) {
+                FieldError fieldError = (FieldError) error;
+                errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+            } else {
+                errors.put(error.getObjectName(), error.getDefaultMessage());
+            }
+        });
+        return new ApiResponseDto<>(ResponseCode.VALIDATION_ERROR.getCode(), errors, ResponseCode.VALIDATION_ERROR.getMessage());
+    }
+}
