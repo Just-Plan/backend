eff569e9eb70aba25633e3a37f5c6f35255ee87c
hstla <ree7243@gmail.com>
2024-01-22 18:43:50 +0900

    feat: jus-46-테스트에 필요한 sql추가

---
+++ src/test/resources/mbti-answer.sql
@@ -0,0 +1,71 @@
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('친한 친구 2명 정도가 적당하지', 'i', 1);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('사람은 많을수록 좋지', 'e', 1);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('한적한 곳에 위치한 프라이빗하고 아늑하게 구성되어 있는 곳', 'i', 2);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('사람들이 많이 찾는 핫플, 저녁에는 다같이 바베큐 파티를 여는 곳!', 'e', 2);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('얼른 집으로 돌아와 침대로 퐁당 "역시 집이 최고야!"', 'i', 3);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('즐거웠던 여행을 추억하기 위해 친구들과 신나는 뒤풀이', 'e', 3);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('되도록 내 친구들이랑만 놀고싶다.', 'i', 4);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('무조건 콜이다', 'e', 4);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('축제 분위기가 괜찮을 것 같긴 한데, 사람이 너무 많으면 피곤할 것 같아.', 'i', 5);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('정말? 축제에 가면 분위기가 엄청날 것 같아! 사람들과 어울리며 즐기는 것도 여행의 묘미잖아. 재미있을 것 같아!', 'e', 5);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('일단 조용히 지도부터 확인해보자. 낯선 곳에서 길을 묻는 건 좀 그래서... 혼자 해결할 수 있으면 좋겠어.', 'i', 6);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('길을 잃었다니 흥미진진하네! 주변 사람들에게 길을 물어보면서 새로운 지역을 더 탐색해볼 수 있겠어. 재미있을 것 같아!', 'e', 6);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('아까는 저쪽으로 가봤으니까 이쪽으로 한번 가보자 왔던 길을 돌아보며 길을 찾는다.', 's', 7);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('이 쪽으로 가면 길이 나오지 않을까? 이쪽 저쪽 나의 감을 믿는다.', 'n', 7);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('오 진짜 어떻게 만든 걸까? 고민한다.', 's', 8);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('와 완전 신기하다!! 이정도면 진짜 외계인이 만든거 아냐? 하고 감탄한다.', 'n', 8);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('내가 자주 만들어서 맛있게 만들 수 있는 음식', 's', 9);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('만들어본 적은 없지만 친구들에게 먹여주고 싶은 신박한 음식', 'n', 9);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('역시 도시랑 다르게 별이 엄청 많이 보이네!', 's', 10);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('우와, 뭐야 방금 별똥별인가? 얼른 소원빌어야지', 'n', 10);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('집에 가서 해야할 일', 's', 11);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('으스스한 귀신 이야기', 'n', 11);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('별은 무슨 인공위성이지', 's', 12);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('방금 지나간 별 UFO가 아닐까?', 'n', 12);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('이런 점은 좀 고쳐줬으면 좋겠어', 't', 13);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('진짜 나 너무 서운하다 ㅠㅠ', 'f', 13);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('너무 고마워, 기름값은 내가 낼게!', 't', 14);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('진짜 고마워 감동이야! 너가 최고다 이시대의 베스트 드라이버!', 'f', 14);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('그게 어디에 필요해? 사고 안쓴다에 한표', 't', 15);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('오! 좋을 것같은데.. 니가 마음에 들면 뭐..', 'f', 15);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('헐 보험 들었어? 돈 많이 깨지겠다..', 't', 16);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('몸은 어때? 다친곳은? 병원가야 되는 거 아냐?', 'f', 16);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('기념품 가게에 갔는데 내 생각이 왜 나?', 't', 17);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('헐 진짜로 ㅠㅠ? 대박 완전 고마워', 'f', 17);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('금방 도착해 조금만 참아', 't', 18);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('속 많이 안 좋아? 멀미약이라도 줄까?', 'f', 18);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('두 번째로 정해둔 맛집을 이동한다', 'j', 19);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('아까 지나오면서 봤던 마음에 든 가게로 들어간다.', 'p', 19);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('아 늦잠 자버렸네 ㅜ, 빨리 일어나서 여행 일정을 변경한다.', 'j', 20);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('어후 꿀잠 잤다. 늦잠을 잔 김에 오늘 하루는 쉬엄쉬엄 여행을 한다.', 'p', 20);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('그냥 한 잔 마시고 아는 게임 해야겠다', 'j', 21);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('에라이~ 마시면서 배우는 거지!', 'p', 21);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('시간 약속은 기본중에 기본인거 몰라?', 'j', 22);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('중요한 일이 있었을 수도 있지, 왔으면 됐어 가자!', 'p', 22);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('내 실수지만 계획에 없던 일이라 스트레스 받는다..', 'j', 23);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('괜찮아~ 어처피 영수증 있으니 전화해서 찾으면 돼', 'p', 23);
+
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('돈을 냈어도 당연히 청소는 한다.', 'j', 24);
+INSERT INTO mbti_answer (answer, `attribute`, question_id) VALUES('청소까지 내가 낸 비용에 포함되어 있다고 생각한다.', 'p', 24);
---
+++ src/test/resources/mbti.sql
@@ -0,0 +1,16 @@
+INSERT INTO mbti (mbti,title,description) VALUES('intj', '계획 변태', '여행 전부터 이미 완벽한 계획을 세운 당신은, 모든 것을 철저하게 준비하는 계획의 달인이에요. 여행에서의 희열은 계획을 실현하는 데서 오죠.');
+INSERT INTO mbti (mbti,title,description) VALUES('intp', '알쓸번잡', '잡학박사 같은 당신은 여행지의 모든 정보를 파악하고, 비효율을 거부하며, 여행 중에도 지식을 나누는 것을 즐겨요. 쓸데없어 보일지라도 당신에겐 소중한 정보들이죠');
+INSERT INTO mbti (mbti,title,description) VALUES('infj', '충전형 이타주의자', '타인을 먼저 생각하는 당신은, 여행에서도 다른 사람들의 행복을 최우선으로 삼아요. 조용하고 평화로운 여행지에서 진정한 행복을 느끼죠.');
+INSERT INTO mbti (mbti,title,description) VALUES('infp', '낭만적 거절가', '여행을 떠나기 전에는 주저하나, 실제로는 여행을 매우 즐기는 당신. 여행에서의 낭만과 이상을 추구하며, 돌아올 때면 다음 여행을 꿈꾸죠.');
+INSERT INTO mbti (mbti,title,description) VALUES('istj', '광기 어린 계획 실천가', '원칙과 계획을 중시하는 당신은 여행에서도 시간 약속과 계획을 철저히 지키며, 어떤 상황에서도 계획대로 움직이는 신뢰할 수 있는 여행자예요.');
+INSERT INTO mbti (mbti,title,description) VALUES('istp', '모험 변태', '돌발 상황에서도 흥미를 느끼는 당신은, 여행 중 스릴과 모험을 즐기며, 예기치 않은 일이 발생해도 즐거워하는 진정한 모험가예요.');
+INSERT INTO mbti (mbti,title,description) VALUES('isfj', '보살', '모두의 필요를 살피며 조화롭게 여행을 이끄는 당신은, 여행지에서도 따뜻하고 친절한 분위기를 조성해 모두가 즐거운 시간을 보낼 수 있게 해요.');
+INSERT INTO mbti (mbti,title,description) VALUES('isfp', '신밧드', '자유분방하고 낭만적인 당신은, 여행에서도 새로운 경험과 환상을 추구하며, 일상에서 벗어난 비현실적인 모험을 즐겨요.');
+INSERT INTO mbti (mbti,title,description) VALUES('entj', '짱가형', '철저한 계획과 강한 리더십을 가진 당신은, 여행에서도 문제 해결 능력을 발휘해 어떤 상황에서도 여행을 원활하게 이끌어가는 책임감 있는 여행자예요.');
+INSERT INTO mbti (mbti,title,description) VALUES('entp', '박물관 지박령', '새로운 아이디어와 발견에 열정적인 당신은, 여행 중에도 지식을 나누며 모든 것을 재미있고 독특하게 만드는 창의적인 여행자예요.');
+INSERT INTO mbti (mbti,title,description) VALUES('enfj', '도라에몽', '타인에게 영감을 주는 당신은, 여행에서도 카리스마와 열정으로 사람들을 이끌고, 깊은 관계 형성에 중점을 두는 따뜻한 여행자예요.');
+INSERT INTO mbti (mbti,title,description) VALUES('enfp', '핵인싸', '즉흥적이고 열정적인 당신은, 여행에서도 새로운 사람들과의 만남을 즐기며, 어디서든 즐거움을 찾아내는 사교적인 여행자예요.');
+INSERT INTO mbti (mbti,title,description) VALUES('estj', '빨리빨리', '계획적이고 실용적인 당신은, 여행에서도 효율과 질서를 중시하며, 빠르고 정확한 결정으로 여행을 효과적으로 이끄는 실용주의자예요.');
+INSERT INTO mbti (mbti,title,description) VALUES('estp', '고집불통', '즉흥적이고 도전적인 당신은, 여행에서도 새로운 경험을 적극적으로 추구하며, 어떤 상황에서도 자신만의 방식으로 여행을 즐기는 자유로운 영혼이에요.');
+INSERT INTO mbti (mbti,title,description) VALUES('esfj', '광고형 여행자', '친절하고 사교적인 당신은, 여행에서도 모든 순간을 기록하고 공유하며, 주변 사람들과 함께 즐거운 추억을 만들어가는 활기찬 여행자예요.');
+INSERT INTO mbti (mbti,title,description) VALUES('esfp', '합석 장인', '사람들과 어울리는 것을 사랑하는 당신은, 여행에서도 새로운 만남과 즐거운 경험을 추구하며, 어디서나 친구를 만들어 즐거운 시간을 보내는 사교적인 여행자예요.');
