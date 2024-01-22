f04f9eb5aadd44c3e18a02751ff1f296e6745fea
hstla <ree7243@gmail.com>
2024-01-22 11:02:40 +0900

    feat: jus-46-mbti controller 구현 계획 수립

---
+++ src/main/java/com/jyp/justplan/domain/mbti/presentation/MbtiTestController.java
@@ -0,0 +1,18 @@
+package com.jyp.justplan.domain.mbti.presentation;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/mbti")
+public class MbtiTestController {
+
+    // 질문과 답변 리스트 조회
+
+
+
+    // 선택한 답변으로 mbti 결과 조회
+
+
+
+}
