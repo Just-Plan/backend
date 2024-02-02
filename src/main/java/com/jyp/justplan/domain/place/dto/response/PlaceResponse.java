5ddd9c7ee910f7b2fe6dafda0905f6e016cdb506
hstla <ree7243@gmail.com>
2024-02-02 12:55:47 +0900

    feat: jus-74 전체 장소 조회 구현

--- src/main/java/com/jyp/justplan/domain/place/dto/response/PlaceResponse.java
+++ src/main/java/com/jyp/justplan/domain/place/dto/response/PlaceResponse.java
@@ -4,40 +4,30 @@ import com.jyp.justplan.domain.place.domain.Place;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import java.util.Collections;
-import java.util.List;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PlaceResponse {
-
-    private int day;
-    private int orderNum;
-    private Long memoId;
-    private String content;
-    private String color;
     private String name;
     private String formattedAddress;
-    private List<String> types;
+    private String types;
     private double latitude;
     private double longitude;
     private String photoReference;
+    private MemoResponse memo;
+    private int orderNum;
 
     public static PlaceResponse of(Place place) {
         return new PlaceResponse(
-                place.getDay(),
-                place.getOrderNum(),
-                place.getMemo().getId(),
-                place.getMemo().getContent(),
-                place.getMemo().getColor(),
                 place.getGooglePlace().getName(),
                 place.getGooglePlace().getAddress(),
-                Collections.singletonList(place.getGooglePlace().getTypes()),
+                place.getGooglePlace().getTypes(),
                 place.getGooglePlace().getLatitude(),
                 place.getGooglePlace().getLongitude(),
-                place.getGooglePlace().getPhotoReference()
+                place.getGooglePlace().getPhotoReference(),
+                MemoResponse.of(place.getMemo()),
+                place.getOrderNum()
         );
     }
-
 }
--- src/main/java/com/jyp/justplan/domain/place/presentation/GooglePlaceController.java
+++ src/main/java/com/jyp/justplan/domain/place/presentation/GooglePlaceController.java
@@ -2,7 +2,6 @@ package com.jyp.justplan.domain.place.presentation;
 
 import com.jyp.justplan.api.response.ApiResponseDto;
 import com.jyp.justplan.domain.place.application.GooglePlaceService;
-import com.jyp.justplan.domain.place.dto.request.GooglePlaceRequest;
 import com.jyp.justplan.domain.place.dto.response.GooglePlaceResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -35,18 +34,6 @@ public class GooglePlaceController {
         @Parameter(description = "장소 키워드", example = "카페", required = true) @RequestParam String query
     ) {
         List<GooglePlaceResponse> response = placeService.getGooglePlace(query, cityId);
-        return ApiResponseDto.successResponse(response);
-    }
-
-    /*CREATE*/
-    @Operation(summary = "구글 장소 생성", description = "구글 장소를 생성합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "성공",
-                    content = @Content(schema = @Schema(implementation = ApiResponseDto.class))),
-    })
-    @PostMapping
-    public ApiResponseDto<GooglePlaceResponse> createGooglePlace(@RequestBody GooglePlaceRequest googlePlaceRequest) {
-        GooglePlaceResponse response = placeService.createGooglePlace(googlePlaceRequest);
         return ApiResponseDto.successResponse(response);
     }
 }
