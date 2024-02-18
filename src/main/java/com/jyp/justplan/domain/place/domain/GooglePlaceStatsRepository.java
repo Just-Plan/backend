169e9ae40d6a6ccb79a613f2b5dde6e78351d469
hstla <ree7243@gmail.com>
2024-02-18 12:32:26 +0900

    fix: mbti통계 삭제 안되는 로직 수정

--- src/main/java/com/jyp/justplan/domain/place/domain/GooglePlaceStatsRepository.java
+++ src/main/java/com/jyp/justplan/domain/place/domain/GooglePlaceStatsRepository.java
@@ -2,10 +2,16 @@ package com.jyp.justplan.domain.place.domain;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface GooglePlaceStatsRepository extends JpaRepository<GooglePlaceStats, Long> {
 
-    void deleteAllByGooglePlaceId(Long googlePlaceId);
+    @Modifying
+    @Query("DELETE FROM GooglePlaceStats gps WHERE gps.googlePlace.id = :googlePlaceId")
+    void deleteAllByGooglePlaceId(@Param("googlePlaceId") Long googlePlaceId);
+
 
     List<GooglePlaceStats> findAllByGooglePlaceId(Long googlePlaceId);
 }
